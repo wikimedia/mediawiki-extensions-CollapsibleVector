4abdc4babfcc15207b1e87969aaced354a798f88
paladox <thomasmulhall410@yahoo.com>
2015-09-03 20:47:26 +0100

    Update grunt-jscs to 2.1.0
    Change-Id: I3b18d19b1f047c137bcd4d0c69e667ad7d3d00a3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,10 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-jscs' );
-	var conf = grunt.file.readJSON( 'extension.json' );
 
 	grunt.initConfig( {
 		jshint: {
