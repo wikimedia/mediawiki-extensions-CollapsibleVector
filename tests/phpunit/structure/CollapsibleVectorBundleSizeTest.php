21054bedbc9f032c5d15a7a49f0ce87dc1746326
Fomafix <fomafix@googlemail.com>
2025-06-05 08:12:35 +0000

    Use getBundleSizeConfigData instead of getBundleSizeConfig
    Change-Id: I700ef0fefef13cacf5241ba3adbe74b004e8f31a

--- tests/phpunit/structure/CollapsibleVectorBundleSizeTest.php
+++ tests/phpunit/structure/CollapsibleVectorBundleSizeTest.php
@@ -8,7 +8,7 @@ namespace Tests\CollapsibleVector\Structure;
 class CollapsibleVectorBundleSizeTest extends \MediaWiki\Tests\Structure\BundleSizeTestBase {
 
 	/** @inheritDoc */
-	public function getBundleSizeConfig(): string {
+	public static function getBundleSizeConfigData(): string {
 		return dirname( __DIR__, 3 ) . '/bundlesize.config.json';
 	}
 }
